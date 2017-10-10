9b642821a9abce3e39d7c0c3ae88c98fcfcd5907
Shama Hoque <shamahoque@users.noreply.github.com>
2017-10-10 12:25:05 +0600

    Update movieQuery.js

--- movieQuery.js
+++ movieQuery.js
@@ -4,7 +4,7 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" }
 ];
 
 function getMovieTitle( index ){
